0c12689fd245900aebf366f9aa141eac11f4adb5
Rohan Kadkol <rohan-kadkol@users.noreply.github.com>
2020-10-15 16:11:44 -0500

    Temp TODOs for step 2

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:love_calculator_3/widgets/names_container.dart';
 
-// TODO (6): Import the newly downloaded http library.
 import 'package:http/http.dart' as http;
+// TODO (1): Import the dart:convert library
 
 import 'results_screen.dart';
 
@@ -27,33 +27,32 @@ class HomeScreen extends StatelessWidget {
           name1: name1,
           name2: name2,
           onTap: () async {
-            // The first part focuses on downloading the percentage and message.
-            // For that, let's create the url
-            // TODO (1): Create a string variable called "url" to hold the url...
-            //    of our Love Calculator API and the names as two query parameters.
             var url =
                 'https://rapidapi.p.rapidapi.com/getPercentage?fname=${name1.text.trim()}&sname=${name2.text.trim()}';
-
-            // Our api also needs us to pass two headers, a host and a key.
-            // TODO (2): Create a Map of <String, String> to hold the host and key headers.
             Map<String, String> headers = {
               'x-rapidapi-host': 'love-calculator.p.rapidapi.com',
               'x-rapidapi-key':
               '4c18f5885bmsh1e7d3e79b072bc6p1ab2a4jsn3c7c75f09aea'
             };
 
-            // Now we let's make the request to our API.
-            // To do that, we need to include the http package.
-            // Let's search for this package on the official Flutter libraries website: pub.dev
-            // TODO (3): Search for the http package on pub.dev
-            // TODO (4): Copy the library name and the version number.
-            // Now let's add this dependency to our pubspec.yml
-
-            // Now that we have the library, let's use it to make the request to our API
-            // TODO (7): Call the http.get method and pass the url and headers
-            // TODO (8): Print the response to the console to see if everything is working.
             var response = await http.get(url, headers: headers);
+            // Now instead of printing the response, let's pass it to the results screen.
+            // For doing that, we have to decode the response.
+            // To do that, let's import another library, 'dart:convert'
             print(response.body);
+
+            // TODO (2): Call the jsonDecode method of the dart:convert package
+            // TODO (3): Pass the response.body as a parameter
+            // TODO (4): Save the result in a variable called jsonResponse
+            // We now have the jsonResponse parsed as a Map!
+            // Isn't this much easier than having to manually parse it?
+
+            // Now let's launch the results screen and pass the percent and message from jsonResponse
+            // TODO (5): Call Navigator.push()
+            // TODO (6): Pass the context variable as the first parameter
+            // TODO (7): Pass a MaterialPageRoute() as the second parameter
+            // TODO (8): Return ResultsScreen() to the builder of the MaterialPageRoute
+            // TODO (9): Finally, pass the parameters of 'percentage' and 'result' to ResultsScreen()
           },
         ),
       ),
